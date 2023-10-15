282a86f3afda5ab78d6f15f2f303a3eabd6570c9
Minatallla22 <menabashir1964@gmail.com>
2023-10-15 07:48:56 -0700

    done

--- handle_format.c
+++ handle_format.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_format - handle format of the output
  * @format: format string that specified how output should be formatted
@@ -17,35 +16,25 @@ int handle_format(const char *format, va_list args)
 		if (format[h] != '%')
 			my_putchr(format[h]);
 		else if (format[h + 1] == 'c')
-		{
-			my_putchr(va_arg(args, int));
-			h++;
-		}
+		{	my_putchr(va_arg(args, int));
+			h++; }
 		else if (format[h + 1] == 's')
-		{
-			r_val = my_puts(va_arg(args, char *));
+		{	r_val = my_puts(va_arg(args, char *));
 			h++;
-			r_value += (r_val - 1);
-		}
+			r_value += (r_val - 1); }
 		else if (format[h + 1] == 'b')
 		{
 			r_val += (printBinary(va_arg(args, unsigned int))) - 1;
-			h++;
-		}
-		else if (format[h + 1] == 'd' || format[h + 1] == 'i')
+			h++; } else if (format[h + 1] == 'd' || format[h + 1] == 'i')
 		{
 			num = va_arg(args, int);
 			r_val = my_puts(print_int(num));
 			r_value += (r_val - 1);
-			h++;
-		}
-		else if (format[h + 1] == '%')
+			h++; } else if (format[h + 1] == '%')
 		{
 			my_putchr('%');
-			h++;
-		}
-		else
+			h++; } else
 			my_putchr('%');
-	}
+		r_value += 1; }
 	return (r_value);
 }
