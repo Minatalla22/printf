982f62954b2810d208d095e78cb061fafb09b67e
nisreen-salih <fg.nisreen@gmail.com>
2023-10-15 22:02:57 -0700

    1

--- handle_format.c
+++ handle_format.c
@@ -13,14 +13,14 @@ int handle_format(const char *format, va_list args)
 	for (; format[h] != '\0'; h++)
 	{
 		if (format[h] != '%')
-			putchr(format[h]);
+			my_putchr(format[h]);
 		else if (format[h + 1] == 'c')
 		{
-			putchr(va_arg(args, int));
+			my_putchr(va_arg(args, int));
 			h++; }
 		else if (format[h + 1] == 's')
 		{
-			r_val = _puts(va_arg(args, char *));
+			r_val = my_puts(va_arg(args, char *));
 			h++;
 			r_value += (r_val - 1); }
 		else if (format[h + 1] == 'b')
@@ -30,15 +30,15 @@ int handle_format(const char *format, va_list args)
 		else if (format[h + 1] == 'd' || format[h + 1] == 'i')
 		{
 			num = va_arg(args, int);
-			r_val = _puts(print_int(num));
+			r_val = my_puts(print_int(num));
 			r_value += (r_val - 1);
 			h++; }
 		else if (format[h + 1] == '%')
 		{
-			putchr('%');
+			my_putchr('%');
 			h++; }
 		else
-			putchr('%');
+			my_putchr('%');
 		r_value += 1;
 	}
 	return (r_value);
