769ebee7405c4eeb99a99c1f78054442b20feb99
nisreen-salih <fg.nisreen@gmail.com>
2023-10-15 21:57:22 -0700

    1

--- handle_format.c
+++ handle_format.c
@@ -8,27 +8,38 @@
 int handle_format(const char *format, va_list args)
 {
 	unsigned int h = 0, r_value = 0;
-	int r_val = 0;
+	int r_val, num;
 
 	for (; format[h] != '\0'; h++)
 	{
 		if (format[h] != '%')
-			my_putchr(format[h]);
+			putchr(format[h]);
 		else if (format[h + 1] == 'c')
-		{	my_putchr(va_arg(args, int));
+		{
+			putchr(va_arg(args, int));
 			h++; }
 		else if (format[h + 1] == 's')
-		{	r_val = my_puts(va_arg(args, char *));
+		{
+			r_val = _puts(va_arg(args, char *));
 			h++;
 			r_value += (r_val - 1); }
 		else if (format[h + 1] == 'b')
 		{
-			r_val += (printBinary(va_arg(args, unsigned int))) - 1;
-			h++; } else if (format[h + 1] == '%')
-			{
-			my_putchr('%');
-			h++; } else
-				my_putchr('%');
-			r_value += 1; }
+			r_value += (printBinary(va_arg(args, unsigned int))) - 1;
+			h++; }
+		else if (format[h + 1] == 'd' || format[h + 1] == 'i')
+		{
+			num = va_arg(args, int);
+			r_val = _puts(print_int(num));
+			r_value += (r_val - 1);
+			h++; }
+		else if (format[h + 1] == '%')
+		{
+			putchr('%');
+			h++; }
+		else
+			putchr('%');
+		r_value += 1;
+	}
 	return (r_value);
 }
