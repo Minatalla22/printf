015fecfa741e5b02b46c55ae5c7e17af15145e86
nisreen-salih <fg.nisreen@gmail.com>
2023-10-17 03:13:51 -0700

    1

--- handle_format.c
+++ handle_format.c
@@ -17,27 +17,22 @@ int handle_format(const char *format, va_list args)
 		else if (format[h + 1] == 'c')
 		{
 			my_putchr(va_arg(args, int));
-			h++; }
-		else if (format[h + 1] == 's')
+			h++; }	else if (format[h + 1] == 's')
 		{
 			r_val = my_puts(va_arg(args, char *));
 			h++;
-			r_value += (r_val - 1); }
-		else if (format[h + 1] == 'b')
+			r_value += (r_val - 1); }	else if (format[h + 1] == 'b')
 		{
 			r_val += (printBinary(va_arg(args, unsigned int))) - 1;
-			h++; }
-		else if (format[h + 1] == 'd' || format[h + 1] == 'x')
+			h++; }	else if (format[h + 1] == 'd' || format[h + 1] == 'i')
 		{
 			num = va_arg(args, int);
 			r_val = my_puts(print_int(num));
 			r_value += (r_val - 1);
-			h++; }
-		else if (format[h + 1] == '%')
+			h++; }	else if (format[h + 1] == '%')
 		{
 			my_putchr('%');
-			h++; }
-		else
+			h++; }	else
 			my_putchr('%');
 		r_value += 1; }
 	return (r_value);
--- main.h
+++ main.h
@@ -13,4 +13,5 @@ int my_puts(char *string);
 char *print_int(int n);
 int printBinary(unsigned int num);
 int handle_format(const char *format, va_list args);
+int int_print(const char *format, ...);
 #endif
